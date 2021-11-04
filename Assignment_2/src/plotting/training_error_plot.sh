20a158a3a961905c6a2b27002d769438003edaba
VenturaLM <ventura_lucena_99@hotmail.com>
2021-11-04 18:52:27 +0100

    xor test.

--- Assignment_2/src/plotting/training_error_plot.sh
+++ Assignment_2/src/plotting/training_error_plot.sh
@@ -2,9 +2,13 @@
 
 cat << _end_ | gnuplot
 set terminal postscript eps color
-set output "training_error_plot.eps"
+set output "../../results/xor_i1000_l1_h8_e07_m1_f1_s/xor_i1000_l1_h8_e07_m1_f1_s.eps"
 set key right bottom box
 set xlabel "Iterations"
 set ylabel "Training error"
-plot 'seed1.txt' using 1:2 t "Seed 1" w l lw 5, 'seed2.txt' using 1:2 t "Seed 2" w l lw 5, 'seed3.txt' using 1:2 t "Seed 3" w l lw 5
+plot '../../results/xor_i1000_l1_h8_e07_m1_f1_s/seed_1.txt' using 1:2 t "Seed 1" w l lw 5, \\
+'../../results/xor_i1000_l1_h8_e07_m1_f1_s/seed_2.txt' using 1:2 t "Seed 2" w l lw 5, \\
+'../../results/xor_i1000_l1_h8_e07_m1_f1_s/seed_3.txt' using 1:2 t "Seed 3" w l lw 5, \\
+'../../results/xor_i1000_l1_h8_e07_m1_f1_s/seed_4.txt' using 1:2 t "Seed 4" w l lw 5, \\
+'../../results/xor_i1000_l1_h8_e07_m1_f1_s/seed_5.txt' using 1:2 t "Seed 5" w l lw 5
 _end_
